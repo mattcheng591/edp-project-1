14641e1f7d733a16f879d1fb89af969a1528c528
Chris Park <5chrispark5@gmail.com>
2025-03-03 13:54:24 -0500

    Font face

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 let characters = [];
 let matchingCharacters = [];
-const charactersList = document.querySelector("#charactersList")
+const charactersList = document.querySelector("#charactersList");
 
-document.addEventListener('DOMContentLoaded', getCharacters)
+document.addEventListener("DOMContentLoaded", getCharacters);
 
 async function getCharacters() {
-  let url = 'http://localhost:9001/api/characters';
+  let url = "http://localhost:9001/api/characters";
 
   try {
-    const fetchedCharacters = await fetch(url)
-      .then(res => res.json())
+    const fetchedCharacters = await fetch(url).then((res) => res.json());
     characters.push(...fetchedCharacters);
-  }
-  catch (ex) {
+  } catch (ex) {
     console.error("Error reading characters.", ex.message);
   }
-  console.log("All the characters are ", characters)
+  console.log("All the characters are ", characters);
   renderCharacters(characters);
 }
 
 const filterCharacters = () => {
   const searchString = document.querySelector("#searchString").value;
   const re = new RegExp(searchString, "i");
-  matchingCharacters = characters.filter(character => re.test(character.name))
+  matchingCharacters = characters.filter((character) =>
+    re.test(character.name)
+  );
   renderCharacters(matchingCharacters);
-}
+};
 
-const renderCharacters = characters => {
-  const divs = characters.map(character => {
-    const el = document.createElement('div');
-    el.addEventListener('click', () => goToCharacterPage(character.id));
-    el.textContent = character.name;
+const renderCharacters = (characters) => {
+  const divs = characters.map((character) => {
+    const el = document.createElement("div");
+    el.addEventListener("click", () => goToCharacterPage(character.id));
+    el.textContent = character.name.toLowerCase();
     return el;
-  })
-  charactersList.replaceChildren(...divs)
-}
+  });
+  charactersList.replaceChildren(...divs);
+};
 
-const goToCharacterPage = id => window.location = `/character.html?id=${id}`
+const goToCharacterPage = (id) =>
+  (window.location = `/character.html?id=${id}`);
